3bcfec38bca23963133c79ef916a315a06b7469c
Evil <lordcashtugbaski@gmail.com>
2019-06-01 10:19:36 +0100

    First Commit

---
+++ app.js
@@ -0,0 +1,30 @@
+'use strict';
+
+const xpresser = require("xpresser");
+
+xpresser({
+    name: "Xjs App",
+    project: {
+        theme: "bulma"
+    },
+    paths: {
+        // The only Required config.
+        base: __dirname,
+
+        /**
+         * In Xpresser we have special path strings
+         * and base:// simply means xpresser should
+         * use base path.
+         *
+         * See file: xpresser/src/config.ts
+         * For all default Config options.
+         */
+        backend: 'base://',
+
+        /**
+         * By default if not set xjs will use base://backend/routes.js
+         * Since this is not a framework app..
+         */
+        routesFile: 'base://routes.js'
+    }
+});
---
+++ readme.md
@@ -0,0 +1,19 @@
+![Alt text](public/xjs-logo-white.png "Xpresser Logo")
+ 
+Example app that shows a simple view but also has guides in code comments
+
+### Includes 
+Xpresser Default Page using [Bulma.io](https://bulma.io) & [Bootstrap](https://getbootstrap.com)
+Theme can be changed in `app.js`
+
+### Use
+1. Clone Or Download
+2. Run `yarn` or `npm` install. (yarn preferably).
+3. Run `npm run dev` if you have **nodemon** or `npm start` to use **node**
+4. check settings in app.js
+5. Read [Xpresser Documentation](https://www.npmjs.com/package/xpresser)
+
+
+
+#### What next?
+Please Star this on [Github](https://github.com/xpresserjs/new-app.git) :)
